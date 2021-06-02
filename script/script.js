3057dd58258f4746752e59e76d30354bfdc7e6c2
sudonut <paradoxotl@gmail.com>
2021-06-02 17:41:29 -0400

    Added some project links

--- script/script.js
+++ script/script.js
@@ -1,5 +1,5 @@
-document.getElementById("nav-open").addEventListener("click", modalToggle)
-document.getElementById("mobile-modal").addEventListener("click", modalToggle)
+document.getElementById("nav-open").addEventListener("click", modalToggle);
+document.getElementById("mobile-modal").addEventListener("click", modalToggle);
 
 function modalToggle() {
   let mobileModal = document.getElementById("mobile-modal");
