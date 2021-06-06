7edc292d6dfd0662e08f6550bf48d19352101e65
sudonut <paradoxotl@gmail.com>
2021-06-05 21:21:42 -0400

    Added an indicator on the nav bar show the current page the user is on

--- script/script.js
+++ script/script.js
@@ -5,3 +5,26 @@ function modalToggle() {
   let mobileModal = document.getElementById("mobile-modal");
   mobileModal.classList.toggle("open");
 }
+
+const sections = document.querySelectorAll("section[id]");
+
+window.addEventListener("scroll", navHighlight);
+
+function navHighlight() {
+
+  // Get current scroll position
+  let scrollY = window.pageYOffset;
+
+  //Loop through the sections to get height, top and ID values for each
+  sections.forEach(section => {
+    const sectionHeight = section.offsetHeight;
+    const sectionTop = section.offsetTop - 170;
+    sectionId = section.getAttribute("id");
+
+    if ( scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document.querySelector(".navigation a[href*=" + sectionId + "]").classList.add("active");
+    } else {
+      document.querySelector(".navigation a[href*=" + sectionId + "]").classList.remove("active");
+    }
+  })
+}
